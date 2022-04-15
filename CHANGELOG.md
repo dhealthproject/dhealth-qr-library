bfc76af4b424c101bbf00d8f54ecc598746120cc
Gregory Saive <greg@evias.be>
2022-04-15 18:17:14 +0200

    @dhealth/qr-library v1.0.0: QR Codes library for dHealth Network

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# CHANGELOG
+
+All notable changes to this project will be documented in this file.
+
+The changelog format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## [v1.0.0][v1.0.0] - 15-Apr-2022
+
+- Updates dev dependency for `@dhealth/sdk`
+- Updates dev dependency for `@dhealth/hd-wallets`
+- Updates default currency to `dhealth.dhp`
+
+[v1.0.0]: https://github.com/dhealthproject/dhealth-qr-library/releases/tag/v1.0.0
---
+++ src/SignedTransactionQR.ts
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2022-present, Grégory Saive for dHealth Network,
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+// internal dependencies
+import {
+    QRCode,
+    QRCodeDataSchema,
+    QRCodeInterface,
+    QRCodeType
+} from '../index';
+import {INetworkType} from "./sdk";
+import {ISignedTransaction} from "./sdk/ISignedTransaction";
+import {SignedTransactionDataSchema} from "./schemas/SignedTransactionDataSchema";
+
+class SignedTransactionQR extends QRCode implements QRCodeInterface {
+    /**
+     * Construct a signed transaction QR Code out of the
+     * dHealth signed transaction.
+     *
+     * @param singedTransaction
+     * @param type
+     * @param   networkType     {INetworkType}
+     * @param   generationHash         {string}
+     */
+    constructor(/**
+                 * The transaction for the request.
+                 * @var {Transaction}
+                 */
+                public readonly singedTransaction: ISignedTransaction,
+                /**
+                 * The network type.
+                 * @var {NetworkType}
+                 */
+                public readonly networkType: INetworkType,
+                /**
+                 * The chain Id.
+                 * @var {string}
+                 */
+                public readonly generationHash: string,
+                /**
+                 * The QR Code Type
+                 *
+                 * @var {QRCodeType}
+                 */
+                public readonly type: QRCodeType = QRCodeType.SignedTransaction) {
+        super(QRCodeType.SignedTransaction, networkType, generationHash);
+    }
+
+    /**
+     * Parse a JSON QR code content into an AddressQR
+     * object.
+     *
+     * @param   json        {string}
+     * @return  {AddressQR}
+     * @throws  {Error}     On empty `json` given.
+     * @throws  {Error}     On missing `type` field value.
+     * @throws  {Error}     On unrecognized QR code `type` field value.
+     */
+    public static fromJSON(
+        json: string,
+        transactionCreateFromPayload: (payload: string) => ISignedTransaction,
+    ): SignedTransactionQR {
+
+        // create the QRCode object from JSON
+        return SignedTransactionDataSchema.parse(json, transactionCreateFromPayload);
+    }
+
+    /**
+     * The `getTypeNumber()` method should return the
+     * version number for QR codes of the underlying class.
+     *
+     * @see https://en.wikipedia.org/wiki/QR_code#Storage
+     * @return {number}
+     */
+    public getTypeNumber(): number {
+        // Type version for SignedTransaction is Version 40, uses correction level M
+        // This type of QR can hold up to 412 binary bytes.
+        return 40;
+    }
+
+    /**
+     * The `getSchema()` method should return an instance
+     * of a sub-class of QRCodeDataSchema which describes
+     * the QR Code data.
+     *
+     * @return {QRCodeDataSchema}
+     */
+    public getSchema(): QRCodeDataSchema {
+        return new SignedTransactionDataSchema();
+    }
+}
+
+export {SignedTransactionQR};
